4cf4a54f839902dab48393acb1b9959698a0abda
javier encina <encinajavier7@gmail.com>
2019-01-31 14:06:20 -0300

    DropDownList desde un enum sin modelo
    Podemos a partir de un enum, crear un List SelectListItem, esto nos va a permitir crear un DropDownList desde un Enum.

---
+++ CursoASP.NET/CursoASP.NET/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using CursoASP.NET.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CursoASP.NET.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            ViewBag.Propiedad = ToListSelectListItem<ResultadoOperacion>();
+            return View();
+        }
+
+        public List<SelectListItem> ToListSelectListItem<T>()
+        {
+            var t = typeof(T);
+
+            if (!t.IsEnum) { throw new ApplicationException("Tipo debe ser enum"); }
+
+            var members = t.GetFields(BindingFlags.Public | BindingFlags.Static);
+
+            var result = new List<SelectListItem>();
+
+            foreach (var member in members)
+            {
+                var attributeDescription = member.GetCustomAttributes(typeof(System.ComponentModel.DescriptionAttribute),
+                    false);
+                var descripcion = member.Name;
+
+                if (attributeDescription.Any())
+                {
+                    descripcion = ((System.ComponentModel.DescriptionAttribute)attributeDescription[0]).Description;
+                }
+
+                var valor = ((int)Enum.Parse(t, member.Name));
+                result.Add(new SelectListItem()
+                {
+                    Text = descripcion,
+                    Value = valor.ToString()
+                });
+            }
+            return result;
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public bool Empleado { get; set; }
+        public DateTime Nacimiento { get; set; }
+    }
+}
---
+++ CursoASP.NET/CursoASP.NET/Models/ResultadoOperacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace CursoASP.NET.Models
+{
+    public enum ResultadoOperacion
+    {
+        Aprobado = 1,
+        Rechazado = 2,
+        Pendiente = 7,
+        [Description("Pendiente Aprobación")]//Permite reemplazar este texto por el valor para incluir espacios en blanco
+        PendienteAprobacion = 9
+    }
+}
