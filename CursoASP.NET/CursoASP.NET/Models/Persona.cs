24a173697f1d835cdcbe905de0ef13ae63e1256c
javier encina <encinajavier7@gmail.com>
2019-01-31 16:26:31 -0300

    ntroducción al Scaffolding
    Controladores y Vistas | Modelos | Programando en ASP.NET MVC 5

---
+++ CursoASP.NET/CursoASP.NET/Models/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CursoASP.NET.Models
+{
+    public class Pelicula
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public bool EstaEnCArtelera { get; set; }
+        public string Genero { get; set; }
+    }
+}
